58b6af610cacb098a5184c9c3ff8ef7b48168152
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2026-04-24 00:11:21 -0700

    Add files via upload

--- soccer/object_tracking.py
+++ soccer/object_tracking.py
@@ -4,30 +4,41 @@ import numpy as np
 
 
 video_capture = cv2.VideoCapture(0)
-tracker = cv2.TrackerCSRT_create()
-
-
 ret, frame = video_capture.read()
-bbox = cv2.selectROI("Tracking", frame, False)
-tracker.init(frame, bbox)
+tracker = cv2.legacy.MultiTracker_create()
 
-while True:
+boxes = []
+boxes.append(cv2.selectROI('select', frame, False))
+boxes.append(cv2.selectROI('select', frame, False))
+
+for box in boxes:
+    tracker.add(cv2.legacy.TrackerCSRT_create(), frame, box)
+
+
+while (1):
 
     ret, frame = video_capture.read()
-    ret, bbox = tracker.update(frame)
-
-    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-
-    rgb_small_frame = small_frame[:, :, ::-1]
-
+    ret, boxes = tracker.update(frame)
 
     if ret:
+        for box in boxes:
+            x, y, w, h = (int(t) for t in box)
+            img2 = cv2.rectangle(frame, (x, y), (x+w, y+h), 255, 2)
+            img3 = cv2.rectangle(img2, (x, y), (x+w, y+h), 255, 2)
+    cv2.imshow('gfg', img3)
 
-        x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
-        cv2.putText(frame, "Tracking Target", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
-    else:
-        cv2.putText(frame, "Target Lost", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 2)
+    # small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    # rgb_small_frame = small_frame[:, :, ::-1]
+
+
+    # if ret:
+
+    #     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+    #     cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+    #     cv2.putText(frame, "Tracking Target", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
+    # else:
+    #     cv2.putText(frame, "Target Lost", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 2)
 
 
 
