77e1fb4c184eaf2b688473d2cad14ea15c1b08e7
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2026-04-23 21:21:32 -0700

    Add files via upload

--- soccer/object_movement.py
+++ soccer/object_movement.py
@@ -18,8 +18,8 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-greenLower = (0, 0, 0)
-greenUpper = (3, 50, 50)
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
 # if a video path was not supplied, grab the reference
---
+++ soccer/object_tracking.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+# import serial
+
+
+video_capture = cv2.VideoCapture(0)
+tracker = cv2.TrackerCSRT_create()
+
+
+ret, frame = video_capture.read()
+bbox = cv2.selectROI("Tracking", frame, False)
+tracker.init(frame, bbox)
+
+while True:
+
+    ret, frame = video_capture.read()
+    ret, bbox = tracker.update(frame)
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+
+    if ret:
+
+        x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+        cv2.putText(frame, "Tracking Target", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
+    else:
+        cv2.putText(frame, "Target Lost", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 2)
+
+
+
+    # for (top, right, bottom, left), name in zip(face_locations, face_names):
+
+    #     top *= 2
+    #     right *= 2
+    #     bottom *= 2
+    #     left *= 2
+
+    #     cv2.rectangle(frame, (left + 10, top + 80), (right - 10, bottom), (0, 0, 255), 2)
+
+    #     cv2.rectangle(frame, (left + 20, bottom), (right - 20, bottom), (0, 0, 255), cv2.FILLED)
+    #     font = cv2.FONT_HERSHEY_DUPLEX
+    #     cv2.putText(frame, name, (left + 10, bottom - 6), font, 0.55, (50, 50, 50), 1)
+
+       
+
+    cv2.imshow('Tracking', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
