627a477cb49de8460e2410fdee94f626b106c2f7
Dani Stem <57697224+Dani-Stem@users.noreply.github.com>
2026-04-27 22:01:58 -0700

    Add files via upload

--- soccer/object_tracking.py
+++ soccer/object_tracking.py
@@ -5,13 +5,11 @@ import serial
 ser = serial.Serial('/dev/tty.usbmodem101')
 
 video_capture = cv2.VideoCapture(0)
+tracker = cv2.TrackerCSRT_create()
+
 ret, frame = video_capture.read()
-tracker = cv2.legacy.MultiTracker_create()
-
-boxes = []
-boxes.append(cv2.selectROI('select', frame, False))
-boxes.append(cv2.selectROI('select', frame, False))
-
+bbox = cv2.selectROI("Tracking", frame, False)
+tracker.init(frame, bbox)
 
 
 def ShiftR():
@@ -31,39 +29,36 @@ def ShiftL():
     return data
 
 
-for box in boxes:
-    tracker.add(cv2.legacy.TrackerCSRT_create(), frame, box)
-
 
 while (1):
 
     ret, frame = video_capture.read()
-    ret, boxes = tracker.update(frame)
+    ret, bbox = tracker.update(frame)
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
 
     if ret:
-        for box in boxes:
-            x, y, w, h = (int(t) for t in box)
-            img2 = cv2.rectangle(frame, (x, y), (x+w, y+h), 255, 2)
-            img3 = cv2.rectangle(img2, (x, y), (x+w, y+h), 255, 2)
-            img2coords = boxes[0]     
-            img3coords = boxes[1]     
-            img2x = img2coords[0]
-            print(img2x)
-            img3x = img3coords[0]
-            print(img3x)
 
+        x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
+        cv2.rectangle(frame, (x, y), ((x + w), (y + h)), (255, 0, 255), 3, 1)
+        cv2.putText(frame, "Tracking Target", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 255, 0), 2)
+    else:
+        cv2.putText(frame, "Target Lost", (75, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0, 0, 255), 2)
 
-    cv2.imshow('gfg', img3)
+    print(bbox[0])
   
-    if img2x > img3x:
-        print ("move Right by")
-        ShiftR()
+    # if img2x > img3x:
+    #     print ("move Right by")
+    #     ShiftR()
 
-    elif img3x < img2x:
-        print ("move left by")
-        ShiftL()
+    # elif img3x < img2x:
+    #     print ("move left by")
+    #     ShiftL()
+
             
-
     cv2.imshow('Tracking', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
